907d62bf59b3baee9b03f97cb3edfb3c3d1fab42
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-08-09 21:12:03 +0200

    use a dedicated field for the log date

--- logs/post_logs_handler.py
+++ logs/post_logs_handler.py
@@ -25,12 +25,14 @@ class PostLogsHandler(tornado.web.RequestHandler):
         date = datetime.datetime.now()
         index = date.strftime('data-%Y-%m-%d-%H')
 
+        log_date = datetime.datetime.utcfromtimestamp(float(data['date']))
+
         helpers.bulk(
             self.es_client,
             [{
                 '_type': 'data',
                 '_index': index,
-                'date': data['date'],
+                'date': log_date,
                 'message': data['message'],
                 'category': data['category'],
                 'level': data['level'],
