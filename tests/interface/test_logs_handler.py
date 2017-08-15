46ef3fa217bc2c06911e823945b4a6677a781721
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-08-15 07:04:59 +0200

    add check of logs amount into database after post into tests

--- tests/interface/test_logs_handler.py
+++ tests/interface/test_logs_handler.py
@@ -2,9 +2,12 @@
 Tests for POST logs
 """
 
+from datetime import datetime
+
 import pytest
 import requests
-import json
+
+from elasticsearch import Elasticsearch, helpers
 
 BASE_URL = "http://localhost:8000/api/1/service/1"
 
@@ -12,6 +15,17 @@ def test_post_log():
     """
     Checks that post logs returns 200
     """
+    es_client = Elasticsearch(hosts=["elasticsearch"],)
+
+    es_client.delete_by_query(
+        index="data-*",
+        body={
+            "query": {
+                "match_all": {}
+            }
+        }
+    )
+
     json = {
         "logs": [
             {
@@ -27,5 +41,26 @@ def test_post_log():
         BASE_URL + "/logs",
         json=json,
     )
-
     assert response.status_code == 200
+
+    result = es_client.search(
+        index="data-1-2017-08-15",
+        body={
+            "query": {
+                "bool": {
+                    "must": {
+                        "match": {
+                            "service_id": "1"
+                        },
+                        "match": {
+                            "date": datetime.fromtimestamp(1502304972.0)
+                        }
+                    },
+                }
+            }
+        }
+    )
+
+    logs_amount = result["hits"]["total"]
+    assert logs_amount == 1, \
+        "unexpected logs amount, got %s, expected 1" % logs_amount
