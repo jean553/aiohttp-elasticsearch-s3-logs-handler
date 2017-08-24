9c3db70c5a5b087cda1f69a95488336f1a4c84c2
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-08-24 20:38:07 +0200

    add basic version of the script that generates the indices as files and uploads them to s3

---
+++ build_scripts/scripts/create_snapshot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+'''
+Creates a snapshot of for one index.
+'''
+
+def main():
+    '''
+    Script entry point.
+
+    TODO
+    '''
+    pass
+
+if __name__ == '__main__':
+    main()
