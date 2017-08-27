a01779cfd3b00c4ecc573f6358ae7b60ba7dfead
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-08-27 22:05:18 +0200

    replace double quotes by single quotes into the configuration file

--- logs/config.py
+++ logs/config.py
@@ -1,8 +1,8 @@
-"""
+'''
 Loads the configuration from the environment variable,
 prevents the service to start if the variable cannot be found
-"""
+'''
 import os
 
-ELASTICSEARCH_HOSTNAME = os.getenv("ELASTICSEARCH_HOSTNAME")
+ELASTICSEARCH_HOSTNAME = os.getenv('ELASTICSEARCH_HOSTNAME')
 assert ELASTICSEARCH_HOSTNAME is not None
