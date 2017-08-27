a1c455f0b386fda47dea29c6878f8eea232379fe
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-08-27 19:52:53 +0200

    integrate the method that get all indices names

--- build_scripts/scripts/create_snapshot.py
+++ build_scripts/scripts/create_snapshot.py
@@ -2,15 +2,38 @@
 '''
 Creates a snapshot of for one index.
 '''
+import requests
+
+ELASTICSEARCH_ENDPOINT = 'http://elasticsearch:9200'
+
+
+def get_data_indices():
+    '''
+    Returns the list of data indices (data-* format).
+
+    Returns:
+        (list) list of indices names
+    '''
+    indices = requests.get(ELASTICSEARCH_ENDPOINT + '/_aliases')
+    return [
+        index
+        for index
+        in indices.json()
+        if index[:1] != '.'
+    ]
 
 
 def main():
     '''
     Script entry point.
-
-    TODO
     '''
-    pass
+
+    indices = get_data_indices()
+
+    # FIXME: only for debug purposes,
+    # must be deleted
+    for index in indices:
+        print(index)
 
 
 if __name__ == '__main__':
