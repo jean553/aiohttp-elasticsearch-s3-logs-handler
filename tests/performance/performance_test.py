4b4383c5d87e57d5872fd0b8ba3f9208a4d5577e
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-08-29 22:13:29 +0200

    send logs grouped by minutes

--- tests/performance/performance_test.py
+++ tests/performance/performance_test.py
@@ -18,8 +18,11 @@ import aiohttp
 POST_LOGS_ENDPOINT = 'http://localhost:8000/api/1/service/1/logs'
 TSV_FILES_DIRECTORY = '/vagrant/tests/performance/files/'
 
+# 2017-08-29 12:00 to 13:00
 START_TIMESTAMP = 1504008000
 END_TIMESTAMP = 1504011600
+
+SECONDS_INTERVAL = 60
 
 
 async def send_data(json):
@@ -27,7 +30,7 @@ async def send_data(json):
     Posts data to the service.
 
     Args:
-        json(dict) json data to send
+        json(dict): json data to send
     '''
     async with aiohttp.ClientSession() as session:
         async with session.post(
@@ -45,35 +48,50 @@ def main():
     logs_files = os.listdir(TSV_FILES_DIRECTORY)
     loop = asyncio.get_event_loop()
 
-    for file_path in logs_files:
+    start_timestamp = START_TIMESTAMP
+    end_timestamp = start_timestamp + SECONDS_INTERVAL
 
-        with open(TSV_FILES_DIRECTORY + file_path) as logs_file:
+    while start_timestamp < END_TIMESTAMP:
 
-            # FIXME: #45 we read the whole file for now,
-            # but we should only read one minute of logs
-            csv_reader = csv.reader(
-                logs_file,
-                delimiter='\t',
-            )
+        for file_path in logs_files:
 
-            logs = list()
+            with open(TSV_FILES_DIRECTORY + file_path) as logs_file:
 
-            for line in csv_reader:
+                csv_reader = csv.reader(
+                    logs_file,
+                    delimiter='\t',
+                )
 
-                # TODO: #46 investigate why should I do this
-                # as we iterate over the CSV reader
-                log = line[0].split()
+                logs = list()
 
-                logs.append({
-                    'date': log[1],
-                    'level': log[2],
-                    'category': log[3],
-                    'message': log[4],
-                })
+                for line in csv_reader:
 
-            json = {'logs': logs}
+                    # TODO: #46 investigate why should I do this
+                    # as we iterate over the CSV reader
+                    log = line[0].split()
 
-        loop.run_until_complete(send_data(json))
+                    timestamp = int(log[1])
+
+                    if (
+                        timestamp < start_timestamp or
+                        timestamp >= end_timestamp
+                    ):
+                        continue
+
+                    logs.append({
+                        'date': timestamp,
+                        'level': log[2],
+                        'category': log[3],
+                        'message': log[4],
+                    })
+
+                json = {'logs': logs}
+
+                if logs:
+                    loop.run_until_complete(send_data(json))
+
+        start_timestamp += SECONDS_INTERVAL
+        end_timestamp += SECONDS_INTERVAL
 
 
 if __name__ == '__main__':
