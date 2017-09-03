00e8a70c317b0759d701711cabdcd5409ac708a8
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-09-03 14:12:41 +0200

    add terraform single file

---
+++ build_scripts/terraform.tf
@@ -0,0 +1,25 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "service_ami_id" {}
+
+provider "aws" {
+  access_key     = "${var.access_key}"
+  secret_key     = "${var.secret_key}"
+  region         = "${var.region}"
+}
+
+resource "aws_instance" "elk" {
+  ami                 = "ami-0bd0cf6d" # (community) bitnami-elk-5.4.1-0-linux-debian-8-x86_64-hvm-ebs
+  instance_type       = "t2.micro"
+}
+
+resource "aws_instance" "service" {
+  ami                 = "${var.service_ami_id}" # (created by packer.json)
+  instance_type       = "t2.micro"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket              = "tornado-kibana-logs-handler"
+  acl                 = "private"
+}
