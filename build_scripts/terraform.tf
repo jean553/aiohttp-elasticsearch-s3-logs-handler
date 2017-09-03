3c6a0892568e4ace8e3c21b1824d21f7598c30ea
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-09-03 20:41:00 +0200

    add security group allow_ssh in order to allow ingress ssh connections

--- build_scripts/terraform.tf
+++ build_scripts/terraform.tf
@@ -31,3 +31,19 @@ resource "aws_s3_bucket" "bucket" {
   bucket              = "tornado-kibana-logs-handler"
   acl                 = "private"
 }
+
+resource "aws_security_group" "allow_ssh" {
+  name                = "allow_ssh"
+  description         = "allow only inbound SSH traffic"
+
+  ingress {
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name              = "allow_ssh"
+  }
+}
