20ee655357495eb43452b4bbaff12183517890df
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-09-05 00:17:27 +0200

    create whole infra through terraform

--- build_scripts/terraform.tf
+++ build_scripts/terraform.tf
@@ -1,7 +1,7 @@
 variable "access_key" {}
 variable "secret_key" {}
 variable "region" {}
-variable "service_ami_id" {}
+variable "backend_ami_id" {}
 variable "key_name" {}
 
 provider "aws" {
@@ -14,17 +14,19 @@ resource "aws_instance" "elk" {
   ami                 = "ami-0bd0cf6d" # (community) bitnami-elk-5.4.1-0-linux-debian-8-x86_64-hvm-ebs
   instance_type       = "t2.micro"
   key_name            = "${var.key_name}"
+  subnet_id           = "${aws_subnet.vpc_subnet.id}"
 
   tags {
     Name              = "tornado-kibana-logs-handler_elk"
   }
 }
 
-resource "aws_instance" "service" {
-  ami                 = "${var.service_ami_id}" # (created by packer.json)
+resource "aws_instance" "backend" {
+  ami                 = "${var.backend_ami_id}" # (created by packer.json)
   instance_type       = "t2.micro"
   key_name            = "${var.key_name}"
-  security_groups     = ["allow_ssh"]
+  security_groups     = ["${aws_security_group.allow_ssh.id}"]
+  subnet_id           = "${aws_subnet.vpc_subnet.id}"
 
   tags {
     Name              = "tornado-kibana-logs-handler_backend"
@@ -36,9 +38,58 @@ resource "aws_s3_bucket" "bucket" {
   acl                 = "private"
 }
 
+resource "aws_vpc" "vpc" {
+
+  cidr_block          = "10.0.0.0/28"
+
+  tags {
+    Name              = "tornado-kibana-logs-handler_vpc"
+  }
+}
+
+resource "aws_subnet" "vpc_subnet" {
+
+  vpc_id              = "${aws_vpc.vpc.id}"
+  cidr_block          = "10.0.0.0/28"
+
+  tags {
+    Name              = "tornado-kibana-logs-handler_vpc_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "vpc_gateway" {
+
+  vpc_id              = "${aws_vpc.vpc.id}"
+
+  tags {
+    Name              = "tornado-kibana-logs-handler_vpc_gateway"
+  }
+}
+
+resource "aws_route_table" "vpc_route_table" {
+  
+  vpc_id              = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block        = "0.0.0.0/0"
+    gateway_id        = "${aws_internet_gateway.vpc_gateway.id}"
+  }
+
+  tags {
+    Name              = "tornado-kibana-logs-handler_vpc_route_table"
+  }
+}
+
+resource "aws_eip" "backend_eip" {
+
+  instance            = "${aws_instance.backend.id}"
+  vpc                 = true
+}
+
 resource "aws_security_group" "allow_ssh" {
   name                = "allow_ssh"
   description         = "allow only inbound SSH traffic"
+  vpc_id              = "${aws_vpc.vpc.id}"
 
   ingress {
     from_port         = 22
