fe4469b516df66b50eedb97d54fd9abac510d032
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-09-19 22:24:16 +0200

    integrate a first version of the test that checks that log is found when searched between given hours

--- tests/interface/test_pipeline.py
+++ tests/interface/test_pipeline.py
@@ -380,3 +380,48 @@ def test_get_different_minute_from_identical_index_from_es():
     )
     assert response.status_code == 200
     assert len(response.json()['logs']) == 0
+
+
+def test_get_different_hour_from_identical_index_from_es():
+    '''
+    Posts one log and tries to get it using a range including the log hour,
+    tries again with a range excluding the log hour
+    '''
+    _empty_s3_bucket()
+
+    es_client = Elasticsearch([ELASTICSEARCH_HOSTNAME])
+    remove_all_data_indices(es_client)
+    time.sleep(WAIT_TIME)
+
+    # send the first log on August 9, 2017 06:56:12 pm
+
+    first_log_message = 'a first log message'
+    first_log_level = 'a low level'
+    first_log_category = 'a first category'
+    first_log_timestamp = 1502304972
+
+    first_json = {
+        'logs': [
+            {
+                'message': first_log_message,
+                'level': first_log_level,
+                'category': first_log_category,
+                'date': str(first_log_timestamp),
+            }
+        ]
+    }
+
+    response = requests.post(
+        BASE_URL + '/logs',
+        json=first_json,
+    )
+    assert response.status_code == 200
+    time.sleep(WAIT_TIME)
+
+    # verifies the log is found if the GET range includes the log hour
+
+    response = requests.get(
+        '%s/logs/2017-08-09-18-56-10/2017-08-09-18-56-15' % (BASE_URL),
+    )
+    assert response.status_code == 200
+    assert len(response.json()['logs']) == 1
