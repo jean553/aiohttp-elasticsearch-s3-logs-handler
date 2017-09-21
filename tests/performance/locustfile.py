749578fdd48b8333e463ac7f394fa54a103b3b7d
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-09-21 21:54:55 +0200

    integrate a locustfile

---
+++ tests/performance/locustfile.py
@@ -0,0 +1,54 @@
+'''
+Locust file for loading tests purposes.
+'''
+from locust import HttpLocust, TaskSet, task
+
+RANGE_INTERVAL = 3600
+
+
+class WebsiteTasks(TaskSet):
+    '''
+    Set of tasks.
+    '''
+
+    # August 9, 2017 06:56:12 pm
+    timestamp = 1502304972
+
+    @task(1)
+    def post_logs(self):
+        '''
+        Post some logs.
+        '''
+        self.timestamp += 1
+
+        self.client.post(
+            '/api/1/service/1/logs',
+            json={
+                'logs': [
+                    {
+                        'message': 'a log message',
+                        'level': 'a low level',
+                        'category': 'a category',
+                        'date': str(self.timestamp),
+                    }
+                ]
+            }
+        )
+
+    @task(1)
+    def get_logs(self):
+        '''
+        Get some logs.
+        '''
+        self.client.get(
+            '/api/1/service/1/logs/2017-08-09-06-50-00/2017-08-09-10-00-00'
+        )
+
+
+class WebsiteUser(HttpLocust):
+    '''
+    User simulation.
+    '''
+    task_set = WebsiteTasks
+    min_wait = 500
+    max_wait = 1000
