479bbc0727c6c1addda2c8422aba73bf49d288ed
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-09-24 16:18:29 +0200

    put elasticsearch search feature into a dedicated function

--- logs/get_logs_handler.py
+++ logs/get_logs_handler.py
@@ -34,27 +34,14 @@ def get_log_to_string(log: Any) -> str:
     return str(log['_source']).replace("'", '"')
 
 
-async def get_logs(
-    request: web.Request,
-    es_client: Elasticsearch,
+async def get_logs_from_elasticsearch(
+    service_id: int,
+    start_date: str,
+    end_date: str,
 ):
     '''
-    Sends back logs according to the given dates range and service.
+    Coroutine that returns the first page of logs from ES.
     '''
-    service_id = request.match_info.get('id')
-    start_date = request.match_info.get('start')
-    end_date = request.match_info.get('end')
-
-    start = datetime.strptime(
-        start_date,
-        API_DATE_FORMAT,
-    )
-
-    end = datetime.strptime(
-        end_date,
-        API_DATE_FORMAT,
-    )
-
     async with aiohttp.ClientSession() as session:
         with async_timeout.timeout(ELASTICSEARCH_REQUESTS_TIMEOUT_SECONDS):
             async with session.get(
@@ -86,7 +73,35 @@ async def get_logs(
                     }
                 }
             ) as response:
-                result = await response.json()
+                return await response.json()
+
+
+async def get_logs(
+    request: web.Request,
+    es_client: Elasticsearch,
+):
+    '''
+    Sends back logs according to the given dates range and service.
+    '''
+    service_id = request.match_info.get('id')
+    start_date = request.match_info.get('start')
+    end_date = request.match_info.get('end')
+
+    start = datetime.strptime(
+        start_date,
+        API_DATE_FORMAT,
+    )
+
+    end = datetime.strptime(
+        end_date,
+        API_DATE_FORMAT,
+    )
+
+    result = await get_logs_from_elasticsearch(
+        service_id,
+        start_date,
+        end_date,
+    )
 
     stream = web.StreamResponse()
     stream.content_type = 'application/json'
