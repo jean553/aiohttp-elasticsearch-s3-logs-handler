9b9e29460a16f4781e3358340dbc3037979b5148
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-10-01 18:55:13 +0200

    create a security group for the backend instance that allows the http port and protocol

--- build_scripts/terraform.tf
+++ build_scripts/terraform.tf
@@ -32,6 +32,7 @@ resource "aws_instance" "backend" {
   key_name            = "${var.key_name}"
   security_groups     = [
                             "${aws_security_group.allow_ssh.id}",
+                            "${aws_security_group.allow_http.id}",
                             "${aws_security_group.allow_all_outbound.id}"
                         ]
   subnet_id           = "${aws_subnet.vpc_subnet.id}"
@@ -116,6 +117,25 @@ resource "aws_security_group" "allow_ssh" {
 
   tags {
     Name              = "allow_ssh"
+  }
+}
+
+resource "aws_security_group" "allow_http" {
+  name                = "allow_http"
+  description         = "allow only inbound HTTP traffic"
+  vpc_id              = "${aws_vpc.vpc.id}"
+
+  ingress {
+    from_port         = 80
+    to_port           = 80
+    protocol          = "http"
+
+    #FIXME: bad practice, a bastion is better
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name              = "allow_http"
   }
 }
 
