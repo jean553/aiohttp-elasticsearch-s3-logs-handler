d2bce67435e339764ffe60fa7934d43dac92b3d3
Dev Intern AI <dev-intern-ai@dev.ai>
2024-11-28 11:40:05 +0100

    add some comments into the POST handler to explain what it does

--- logs/post_logs_handler.py
+++ logs/post_logs_handler.py
@@ -12,7 +12,14 @@ async def post_logs(
     es_client: Elasticsearch,
 ):
     '''
-    Save sent logs into ElasticSearch.
+    Handle POST request to save logs into Elasticsearch.
+    
+    This function processes incoming log data, formats it for Elasticsearch,
+    and performs a bulk insert operation.
+    
+    Args:
+        request (web.Request): The incoming HTTP request containing log data.
+        es_client (Elasticsearch): The Elasticsearch client for data insertion.
     '''
     data = await request.json()
     logs = data['logs']
@@ -20,6 +27,7 @@ async def post_logs(
     service_id = request.match_info.get('id')
 
     for log in logs:
+        # Generate the index name based on the log date and service ID
 
         # TODO: #125 almost everytime, indices have the same day,
         # so this is superfluous to generate the index for each log;
@@ -27,6 +35,7 @@ async def post_logs(
         log_date = datetime.utcfromtimestamp(float(log['date']))
         index = log_date.strftime('data-{}-%Y-%m-%d'.format(service_id))
 
+        # Augment each log entry with additional metadata
         log.update(
             {
                 '_type': 'logs',
@@ -36,6 +45,7 @@ async def post_logs(
         log['_index'] = index
         log['date'] = log_date
 
+    # Perform bulk insert of logs into Elasticsearch
     helpers.bulk(
         es_client,
         logs,
